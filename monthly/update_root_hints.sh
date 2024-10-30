1c989c7d138931a18593e1d65abc7f707959bde4
Ahmet BARIŞ <barisahmet@gmail.com>
2024-10-30 18:57:55 +0300

    Update update_root_hints.sh

--- monthly/update_root_hints.sh
+++ monthly/update_root_hints.sh
@@ -4,8 +4,8 @@ cd /tmp
 
 wget -O root.hints.new https://www.internic.net/domain/named.root
 
-md5Old=($(md5sum /var/lib/unbound/root.hints | cut -d ' ' -f 1))
-md5New=($(md5sum root.hints.new | cut -d ' ' -f 1))
+md5Old=$(md5sum /var/lib/unbound/root.hints | cut -d ' ' -f 1)
+md5New=$(md5sum root.hints.new | cut -d ' ' -f 1)
 
 echo Existing root.hints: $md5Old
 echo New root.hints: $md5New
